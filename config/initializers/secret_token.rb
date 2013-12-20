adcc90f7472adf99d17d7b38744b422a9f7c1ee5
ravzski <rav_bay@yahoo.com>
2013-12-21 05:07:46 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TurbolinksSlimTest::Application.config.secret_key_base = '59d72d772c3222002468abab3915a4530e4392abb93483c63ff4c89b430a1c1588f954e740d1900f17b4b06d90e9843b8a19ba3175fcb5c54ab344098788d41b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TurbolinksSlimTest::Application.config.session_store :cookie_store, key: '_turbolinks_slim_test_session'
